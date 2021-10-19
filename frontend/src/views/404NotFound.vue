b6d53c0e5f163bb10ec8cc779e935021a53f0274
Harry-Lees <harry.lees@gmail.com>
2021-10-19 21:43:18 +0100

    added catch-all 404 page for unrecognized routes

---
+++ frontend/src/views/404NotFound.vue
@@ -0,0 +1,22 @@
+<template>
+    <central-floating-layout>
+        <div class="flex">
+            <h1 class="text-7xl font-bold text-blue-700 border-r p-4">404</h1>
+            
+            <div class="p-4 space-y-2">
+                <h1 class="text-6xl font-bold text-gray-900">Page Not Found</h1>
+                <p class="text-gray-700">Please check the URL in the address bar and try again.</p>
+            </div>
+        </div>
+    </central-floating-layout>
+</template>
+
+<script>
+import CentralFloatingLayout from "@/layouts/CentralFloatingLayout.vue";
+
+export default {
+    components: {
+        CentralFloatingLayout,
+    }
+}
+</script>
