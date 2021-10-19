aeac06023c6b5d9f4b4e708a403fee1e8ab9c50e
Harry-Lees <harry.lees@gmail.com>
2021-10-19 21:41:44 +0100

    added support for new routes and a 404 error page

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -12,6 +12,25 @@ const routes = [
         name: "Play",
         component: () => import("@/views/Game.vue"),
     },
+    {
+        path: "/settings",
+        name: "Settings",
+        component: () => import("@/views/Settings.vue"),
+    },
+    {
+        path: "/login",
+        name: "Login",
+        component: () => import("@/views/Login.vue"),
+    },
+    {
+        path: "/register",
+        name: "Register",
+        component: () => import("@/views/Register.vue"),
+    },
+    {
+        path: '/:catchAll(.*)',
+        component: () => import("@/views/404NotFound.vue"),
+    },
 ]
 
 const router = createRouter({
