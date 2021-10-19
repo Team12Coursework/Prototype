091a0853de2cb4cc7435fab6caef9b4665fe326d
Harry-Lees <harry.lees@gmail.com>
2021-10-19 13:24:01 +0100

    reworked layout slightly

--- frontend/src/views/Game.vue
+++ frontend/src/views/Game.vue
@@ -1,18 +1,29 @@
 <template>
     <basic-layout>
-        <game-board />
+        <div class='flex w-full justify-center space-x-6'>
+            <div class="w-full max-w-5xl">
+                <game-board class="w-full" />
+                <tile-holder class="border shadow-sm mt-4" />
+            </div>
+
+            <chatbox />
+        </div>
     </basic-layout>
 </template>
 
 <script>
-import BasicLayout from '@/layouts/BasicLayout.vue';
-import GameBoard from '@/components/GameBoard.vue';
+import BasicLayout from "@/layouts/BasicLayout.vue";
+import TileHolder from "@/components/TileHolder.vue";
+import GameBoard from "@/components/GameBoard.vue";
+import Chatbox from "@/components/Chatbox.vue";
 
 export default {
     name: "Home",
     components: {
         BasicLayout,
+        TileHolder,
         GameBoard,
+        Chatbox,
     }
 };
 </script>
