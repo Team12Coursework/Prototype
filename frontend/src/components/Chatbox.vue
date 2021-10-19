cb45431fcec08b21f5ba152b49a1c888c01613f4
Harry-Lees <harry.lees@gmail.com>
2021-10-19 13:21:47 +0100

    added basic ChatBox component, standin for proper component

---
+++ frontend/src/components/Chatbox.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="border p-2 rounded w-1/2 flex flex-col justify-between h-96">
+        <div class="flex flex-col">
+            <span><b class="text-blue-500">You:</b> Hello</span>
+            <span><b class="text-red-500">Opponent:</b> Hello</span>
+        </div>
+        <input class="border rounded p-2" type="text" placeholder="Chat here" />
+    </div>
+</template>
