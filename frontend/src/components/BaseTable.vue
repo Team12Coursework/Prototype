f88bcbf0168752bf7da0684682f23b1bce8f3940
Harry-Lees <harry.lees@gmail.com>
2021-10-19 13:20:55 +0100

    added BaseTable for leaderboard and friend list

---
+++ frontend/src/components/BaseTable.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="border rounded shadow-md mt-4">
+        <table class="min-w-full divide-y divide-gray-200">
+            <thead class="bg-gray-50">
+                <tr>
+                    <th v-for="heading in headings" :key="heading" scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">{{ heading }}</th>
+                </tr>
+            </thead>
+
+            <tbody class="bg-white divide-y divide-gray-200">
+                <slot>
+                </slot>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        headings: {
+            required: true,
+            type: Array,
+        },
+    }
+}
+</script>
