20f1769114c1019a25ddc8a4557624638d8592f2
Harry-Lees <harry.lees@gmail.com>
2021-10-19 21:41:29 +0100

    changed link from Profile -> Settings

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -18,8 +18,8 @@
 
                         <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
                             <div v-if="dropdownOpen"  class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
-                                <router-link to='/profile' class="bg-white hover:bg-gray-100 block px-4 py-2 text-sm text-gray-700">Your Profile</router-link>
-                                <router-link to='/logout' class="bg-white hover:bg-gray-100 block px-4 py-2 text-sm text-gray-700">Logout</router-link>
+                                <router-link to='/settings' class="bg-white hover:bg-gray-100 block px-4 py-2 text-sm text-gray-700">Settings</router-link>
+                                <router-link to='/logout' class="bg-white hover:bg-gray-100 block px-4 py-2 text-sm text-red-700">Logout</router-link>
                             </div>
                         </transition>
                     </div>
