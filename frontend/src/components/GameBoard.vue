341a24631f1e8b6b460d930fef134d56438755a3
Harry-Lees <harry.lees@gmail.com>
2021-10-19 13:23:15 +0100

    simplified GameBoard and added some basic dynamic functionality

--- frontend/src/components/GameBoard.vue
+++ frontend/src/components/GameBoard.vue
@@ -1,25 +1,17 @@
 <template>
-    <div class='flex flex-col items-center justify-center'>
-        <div>
-            <div class='flex justify-center items-center space-x-2 font-bold text-2xl text-gray-600 mb-6'>
-                <div v-for="(row, i) in board" :key='row' class='space-y-2'>
-                    <div v-for="(cell, j) in row" :key='cell'>
-                        <div id='cell' @click='selected = 9 * i + j' class='bg-gray-100 rounded-md flex justify-center items-center w-16 h-16 cursor-pointer' v-bind:class='{ "bg-blue-400 text-white": (9 * i + j) == selected }'>
-                            <span>0</span>
-                        </div>
-                    </div>
-                </div>
+    <div class="flex space-x-2 justify-center w-full">
+        <div v-for="i in 15" :key="i" class="flex flex-col space-y-2 w-full">
+            <div v-for="j in 15" :key="j" class="flex bg-gray-100 w-full rounded justify-center items-center font-semibold" style="aspect-ratio: 1;">
+                {{ j }}
             </div>
         </div>
     </div>
 </template>
 
 <script>
-    export default {
-        data() {
-            return {
-                board: Array(10).fill(0).map(() => Array(10).fill(0)),
-            }
-        },
+export default {
+    mounted() {
+        console.log("hello");
     }
+}
 </script>
