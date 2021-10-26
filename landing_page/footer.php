c43458221df0b38430e158c95017e053c372bb08
zaieedh <zaieedh3755@gmail.com>
2021-10-26 15:00:21 +0100

    added basic landing page files

---
+++ landing_page/footer.php
@@ -0,0 +1,11 @@
+
+div class="end-footer">
+	<p> Aston St, Birmingham B4 7ET </p>
+	<p>Phone +44 0121 204 3000 </p>
+	<p> enablingteam@aston.ac.uk.</p>
+	<div class="Socialmedias">
+  		<a href="https://en-gb.facebook.com/"><img src= "img/facebook.png"></a>
+  		<a href="https://twitter.com/"><img src="img/twitter.png"></a>
+  		<a href="https://www.instagram.com/"><img src="img/instagram.png"></a>
+	</div>
+</div>
