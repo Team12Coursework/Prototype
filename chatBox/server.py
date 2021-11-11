8526f37351e57765102032401cfae7cd598cb7f6
Sufyan <200103608@aston.ac.uk>
2021-11-11 23:20:38 +0000

    uncompleted version of the server script for the chatbox

--- chatBox/server.py
+++ chatBox/server.py
@@ -1,11 +1,22 @@
 import socket
 import threading
+import select
 
 socks = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host = socket.gethostbyname(socket.gethostname())
 port = 69500
-address = (host,port)
+server_address = (host,port)
 
-socks.bind(address)
-socks.listen()
-print("hello world")
+socks.bind(server_address)
+listening = socks.listen()
+
+clients = ()
+
+def serverListen() :
+    while listening == True:
+        connection, client_address = socks.accept()
+        thread1 = threading.Thread(target=clientHandler, args=(connection, client_address))
+
+
+def clientHandler(connection, client_address):
+    pass
