acf0604bd96134393e4932a7f43d08c54d6a79a3
Harry <harry.lees@gmail.com>
2021-11-14 14:33:55 +0000

    added description of main file

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,6 +4,9 @@ import router from '@/router/index.js';
 import './assets/tailwind.css'
 import store from './store'
 
+// main file registers all components and plugins before mounting the Vue application to the #app
+// div found in public/index.html
+
 const app = createApp(App).use(store).use(router);
 
 app.use(router);
