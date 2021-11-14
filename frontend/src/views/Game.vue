231d9fc64feb1dd6b2d71dd9565d667cdb1fbdb3
Harry <harry.lees@gmail.com>
2021-11-14 11:56:51 +0000

    fixed styling and added a unique ID to each tile

--- frontend/src/views/Game.vue
+++ frontend/src/views/Game.vue
@@ -1,15 +1,14 @@
 <template>
-        <main class="w-full flex min-h-screen h-screen border flex-col p-6 space-y-4">
-            <div class="w-full h-full border space-x-2 flex">
+        <navbar />
+        <main class="w-1/2 flex min-h-screen h-screen border flex-col p-6 space-y-4">
+            <div class="w-full h-full space-x-2 flex">
                 <div class="space-y-2 w-full h-full flex flex-col" v-for ="i in 15" :key ="i">
-                    <Board class="bg-green-500 w-full min-h-24 h-full p-2 flex items-center justify-center" v-for ="j in 15" :key ="j" id="board-2" />
+                    <Board class="bg-green-500 w-full min-h-24 h-full p-1 flex items-center justify-center" v-for ="j in 15" :key ="j" :id="loc(i, j)" />
                 </div>
             </div>
 
-            <Board class="w-full border p-2 bg-gray-500 h-32 flex space-x-2" id="board-1">
-                <Card v-for="i in 6" :key="i" :id="i" draggable="true">
-                    <p>A</p>
-                </Card>
+            <Board class="w-full border p-2 bg-gray-500 h-32 flex space-x-2" :id="0">
+                <Card v-for="i in 6" :key="i" :id="i" :letter="alphabet[i]" :draggable="true" />
             </Board>
         </main>
 </template>
@@ -17,12 +16,30 @@
 <script>
 import Board from '@/components/Board.vue'
 import Card from '@/components/Card.vue'
+import Navbar from '@/components/Navbar.vue'
+
+const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 export default{
     name: 'game',
+
+    data() {
+        return {
+            alphabet,
+        }
+    },
+
     components:{
         Board,
-        Card
-    }
+        Card,
+        Navbar,
+    },
+
+    methods: {
+        loc(x, y) {
+            // method to return the square ID given the x, y position on the board
+            return 15 * x + y
+        },
+    },
 }
 </script>
