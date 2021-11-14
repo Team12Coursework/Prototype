78a7802cee9c5e8c1c038bf3430d6bb51436344c
Harry <harry.lees@gmail.com>
2021-11-14 13:47:27 +0000

    added Vuex broadcast on piece drop

--- frontend/src/components/Board.vue
+++ frontend/src/components/Board.vue
@@ -16,10 +16,29 @@ export default{
 
     methods: {
         drop(e) {
+            // function to handle dropping the piece onto the board.
+            // as soon as a piece is dropped, it will trigger an event
+            // which will display the next turn and reset buttons.
+
             const card_id = e.dataTransfer.getData('card_id');
             const card = document.getElementById(card_id);
+            const letter = card.childNodes[0].innerHTML;
 
-            console.log(this.id, card_id);
+            let x = (this.id % 15) - 1;
+            let y = Math.floor(this.id / 15) - 1;
+
+            let piece = {
+                x: x,
+                y: y,
+                id: card_id,
+                letter: letter,
+            };
+
+            this.$store.dispatch("board/movePiece", piece).then(
+                () => {
+                    console.log("piece moved successfully");
+                }
+            )
 
             card.style.display = "block";
             e.target.appendChild(card);
