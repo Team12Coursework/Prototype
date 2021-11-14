219c5bbf83d40b29e7d7ca5250838f6367a8065a
zaieedh <zaieedh3755@gmail.com>
2021-11-14 00:08:47 +0000

    addedChanges

---
+++ landing_page/bottomfooter.php
@@ -0,0 +1,38 @@
+
+<body>
+<footer>
+<div class ="container">
+<div class ="row">
+<div class = "footer-col">
+<h3>help</h3>
+<ul>
+    <li><a href="Support.html">Support</a></li>
+    <li><a href="GameRules.html">Game rules</a></li>
+</ul>
+
+  </div>
+  <div class ="footer-col">
+	<h3>get help</h3>
+<ul>
+    <li><a href="about-us.html">About Us</a></li>
+    <li><a href="Contact.html">Contact</a></li>
+</ul>
+  </div>
+  <div class = "footer-col">
+	<h3>Follow us</h3>
+<ul>
+
+     <li><a href="FAQ.html">FAQ</a></li>
+     <li><a href="Terms of Service.html">Terms of Service</a></li>
+</ul>
+</div>
+<div class= "footer-col">
+<h3> Follow us</h3>
+<div class="socialmedias">
+<a href="https://en-gb.facebook.com/"><img src= "facebook.png"></a>
+<a href="https://twitter.com/"><img src="twitter.png"></a>
+
+  </div>
+</footer>
+</body>
+</html>
