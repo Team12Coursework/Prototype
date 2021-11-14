ee7cd4f99d20d87083a92f11fa034de2a2cdf320
Harry <harry.lees@gmail.com>
2021-11-14 15:32:56 +0000

    changed initialState to look for user in localStorage

--- frontend/src/store/auth.module.js
+++ frontend/src/store/auth.module.js
@@ -1,5 +1,5 @@
 import AuthService from '@/services/auth.service';
-const user = JSON.parse(localStorage.getItem('token'));
+const user = JSON.parse(localStorage.getItem('user'));
 
 const initialState = user
   ? { status: { loggedIn: true }, user }
