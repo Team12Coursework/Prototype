092fa69fed1ad741f32a55672e0096ce22f49fc8
Harry <harry.lees@gmail.com>
2021-11-14 11:54:55 +0000

    added prop types and fixed some formatting

--- frontend/src/components/Card.vue
+++ frontend/src/components/Card.vue
@@ -1,23 +1,33 @@
 <template>
-    <div 
-        :id="id"
-        :draggable="draggable"
-        class="font-bold w-full flex items-center justify-center bg-blue-500 text-white border cursor-pointer"
-        @dragstart="dragStart"
-        @dragover.stop
+    <div :id="id" :draggable="draggable"
+        class="font-bold w-full h-full flex items-center justify-center bg-gray-300 text-gray-900 cursor-pointer rounded"
+        @dragstart="dragStart" @dragover.stop
     >
-        <p class="w-full text-center">
-            <slot></slot>
+        <p class="w-full h-full flex items-center justify-center">
+            {{ letter }}
         </p>
     </div>
 </template>
 
 <script>
-
 export default{
-    props: ['id', 'draggable'],
-    methods:{
-        dragStart: e => {
+    props: {
+        id: {
+            type: Number,
+            required: true,
+        },
+        draggable: {
+            type: Boolean,
+            required: true,
+        },
+        letter: {
+            type: String,
+            required: true,
+        },
+    },
+    
+    methods: {
+        dragStart(e) {
             const target = e.target;
 
             e.dataTransfer.setData('card_id', target.id);
@@ -25,8 +35,7 @@ export default{
             setTimeout(() =>{
                 target.style.display = "none";
             }, 0);
-        }
+        },
     }
 }
-    
 </script>
