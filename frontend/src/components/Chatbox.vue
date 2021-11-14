d92f72ede5232bdd5c85f1b9bc6f90d1ad3ef0f7
Harry <harry.lees@gmail.com>
2021-11-14 15:01:23 +0000

    added ability to change who sends message on chatbox

--- frontend/src/components/Chatbox.vue
+++ frontend/src/components/Chatbox.vue
@@ -1,14 +1,14 @@
 <template>
     <!-- Chatbox component used for the Chat functionality in the game screen -->
 
-    <form @submit="onSubmit" class="add-form">
+    <form @submit="sendMessage" class="add-form">
         <div class="border p-2 rounded flex flex-col justify-between h-96 w-90/100">
             <div class="flex flex-col">
                 <div>
-                    <p v-for="message in messages" :key="message">{{ message }}</p>
+                    <p v-for="message in messages" :key="message"><span class="font-bold text-blue-500">{{ message.sender }}:</span> {{ message.message }}</p>
                 </div>
             </div>
-            <input class="border rounded p-2" type="text" v-model="text" placeholder="Chat here" />
+            <input class="border rounded p-2" type="text" v-model="text" placeholder="Your message here..." />
         </div>
     </form>
 </template>
@@ -24,14 +24,14 @@ export default {
     },
 
     methods: {
-        getMessages(message) {
+        addMessage(message) {
             // dummy function provides functionality until the API is finished
             // this will be removed, and an API call will be added to ensure chat messages
             // are broadcast to the other users.
             this.messages = [... this.messages, message]
         },
 
-        onSubmit(e) {
+        sendMessage(e) {
             e.preventDefault()
 
             if(!this.text) {
@@ -39,7 +39,10 @@ export default {
                 return
             }
 
-            this.getMessages(this.text)
+            this.addMessage({
+                message: this.text,
+                sender: "you"
+            })
             this.text = ''
         }
     }
