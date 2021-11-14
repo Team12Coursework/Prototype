8ab84628be60396110cc487d10a66a1816b53727
Harry <harry.lees@gmail.com>
2021-11-14 13:48:36 +0000

    added Vuex store for board state

---
+++ frontend/src/store/board.module.js
@@ -0,0 +1,57 @@
+const initialState = {
+    board: [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ],
+    turn: 1,
+    previousBoard: null,
+    placed: false,
+}
+
+export const board = {
+    namespaced: true,
+    state: initialState,
+
+    actions: {
+        movePiece({ commit, state }, piece) {
+            let newBoard = state.board;
+            newBoard[piece.y][piece.x] = {
+                id: piece.id,
+                letter: piece.letter
+            };
+            commit('pieceMovedSuccess', newBoard);
+        },
+
+        nextTurn({ commit }) {
+            commit('nextTurnSuccess');
+        },
+    },
+
+    mutations: {
+        nextTurnSuccess(state) {
+            // function to increment the turn by one, and to store a copy of the
+            // previous turn's board
+            state.previousBoard = state.board;
+            state.turn += 1;
+            state.placed = false;
+        },
+
+        pieceMovedSuccess(state, board) {
+            state.board = board;
+            state.placed = true;
+        }
+    },
+}
--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,8 +1,10 @@
 import { createStore } from 'vuex'
 import { auth } from '@/store/auth.module.js';
+import { board } from '@/store/board.module.js';
 
 export default createStore({
     modules: {
         auth,
+        board
     },
 })
