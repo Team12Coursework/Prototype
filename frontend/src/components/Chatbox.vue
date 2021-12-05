c1b7ca5656d70d59594b6c74c52aa013ac26b5a4
Harry Lees <harry.lees@gmail.com>
2021-12-05 16:46:30 +0000

    changed Chatbox to accept messages as prop

--- frontend/src/components/Chatbox.vue
+++ frontend/src/components/Chatbox.vue
@@ -4,9 +4,8 @@
     <form @submit="sendMessage" class="add-form">
         <div class="border p-2 rounded flex flex-col justify-between h-96 w-90/100">
             <div class="flex flex-col">
-                <div>
-                    <p v-for="message in messages" :key="message"><span class="font-bold text-blue-500">{{ message.sender }}:</span> {{ message.message }}</p>
-                </div>
+                    {{ messages }}
+                    <!-- <p v-for="message in messages" :key="message"><span class="font-bold text-blue-500">{{ message.sender }}:</span> {{ message.message }}</p> -->
             </div>
             <input class="border rounded p-2" type="text" v-model="text" placeholder="Your message here..." />
         </div>
@@ -16,35 +15,28 @@
 <script>
 export default {
     name: 'Chatbox',
+    props: {
+        messages: Array,
+    },
+
     data() {
         return {
-            messages: [],
-            text: '',
+            text: "",
         }
     },
 
     methods: {
-        addMessage(message) {
-            // dummy function provides functionality until the API is finished
-            // this will be removed, and an API call will be added to ensure chat messages
-            // are broadcast to the other users.
-            this.messages = [... this.messages, message]
-        },
-
         sendMessage(e) {
-            e.preventDefault()
+            e.preventDefault();
 
             if(!this.text) {
-                alert('Please add a comment')
-                return
+                alert('Please add a comment');
+                return;
             }
 
-            this.addMessage({
-                message: this.text,
-                sender: "you"
-            })
-            this.text = ''
+            this.$emit("update:messages", this.text);
+            this.text = ""; // reset the textbox back to an empty string
         }
-    }
+    },
 }
 </script>
