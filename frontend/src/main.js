5c28c9dac7f5f3498edd75a9d6bbf34d1d3d33cf
Harry Lees <harry.lees@gmail.com>
2021-12-05 16:34:11 +0000

    refactored includes in main

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,14 +1,12 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import App from '@/App.vue'
 import router from '@/router/index.js';
-import './assets/tailwind.css'
-import store from './store'
+import '@/assets/tailwind.css'
+import store from '@/store'
 
 // main file registers all components and plugins before mounting the Vue application to the #app
 // div found in public/index.html
 
 const app = createApp(App).use(store).use(router);
-
-app.use(router);
 
 app.mount('#app')
