86f5283abbe000903f42a085879f29c59df1d268
Harry Lees <harry.lees@gmail.com>
2021-12-05 16:36:45 +0000

    added lobby page to allow users to join the game

---
+++ frontend/src/views/Lobby.vue
@@ -0,0 +1,40 @@
+<template>
+    <navbar/>
+    <main class="flex justify-center w-full">
+        <div class="w-full max-w-7xl flex flex-col space-y-12 items-center">
+            <div class="flex flex-col items-center pt-6">
+                <h1 class="text-6xl font-bold">Lobby</h1>
+
+                <p class="mt-4">Invite Friends with the link below</p>
+                <p class="rounded border mt-4 px-4 py-2">localhost:8080{{ link.href }}</p>
+
+                <router-link class="rounded p-2 bg-green-400" :to="link">Go</router-link>
+            </div>
+        </div>
+    </main>
+</template>
+
+<script>
+import { ref, onMounted } from "vue";
+import { useRouter } from "vue-router";
+import Navbar from "@/components/Navbar.vue";
+
+export default {
+    components: {
+        Navbar,
+    },
+
+    setup() {
+        const link = ref("");
+        const getLink = () => {
+            let epoch_time = Date.now();
+            const router = useRouter()
+            link.value = router.resolve({name: 'Play', params: {game_id: epoch_time}});
+        }
+
+        onMounted(getLink);
+
+        return { link }
+    },
+}
+</script>
