03112b35e6e1b6037b767ae78d9ef2016ada3611
Harry Lees <harry.lees@gmail.com>
2021-12-05 16:45:11 +0000

    added basic WebSocket functionality for multiplayer

--- frontend/src/views/Game.vue
+++ frontend/src/views/Game.vue
@@ -7,17 +7,17 @@
                 <div class="w-full flex flex-col space-y-4">
                     <div class="w-full h-full space-x-2 flex">
                         <div class="space-y-2 w-full h-full flex flex-col" v-for ="i in 15" :key ="i">
-                            <Board class="bg-gray-300 w-full min-h-24 h-full p-1 flex items-center justify-center" v-for ="j in 15" :key ="j" :id="loc(j, i)" />
+                            <board class="bg-gray-300 w-full min-h-24 h-full p-1 flex items-center justify-center" v-for ="j in 15" :key ="j" :id="loc(j, i)" />
                         </div>
                     </div>
 
-                    <Board v-if="letters" class="w-full p-2 bg-gray-300 h-32 flex space-x-2" :id="0">
-                        <Card v-for="i in 7" :key="i" :id="i" :letter="letters[i-1]" :draggable="true"/>
-                    </Board>
+                    <board v-if="letters" class="w-full p-2 bg-gray-300 h-32 flex space-x-2" :id="0">
+                        <card v-for="i in 7" :key="i" :id="i" :letter="letters[i-1]" :draggable="true"/>
+                    </board>
                 </div>
 
                 <div class="w-full flex flex-col">
-                    <chatbox />
+                    <chatbox :messages="chatMessages" @update:messages="this.sendChatMessage($event)" />
 
                     <div v-if="piecePlaced" class="flex space-x-2">
                         <button @click="resetBoard" class="cursor-pointer p-2 bg-blue-500 rounded text-white">Reset</button>
@@ -38,13 +38,18 @@ import Scoreboard from '@/components/Scoreboard.vue'
 const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 export default{
-    name: 'game',
+    name: "Game",
 
     data() {
         return {
             alphabet,
             maxId: 0,
             letters: [],
+            chatMessages: [],
+            player: {
+                name: "HarryLees",
+            },
+            socket: null,
         }
     },
 
@@ -79,7 +84,32 @@ export default{
                 this.maxId += 1;
             }
             this.letters = letters;
-        }
+        },
+
+        sendChatMessage(message) {
+            this.socket.send(JSON.stringify({type: "message", "message": message}));
+        },
+
+        receiveChatMessage(message) {
+            this.chatMessages.push(message);
+        },
+
+        processMessage(data) {
+            console.log('called:', data);
+            console.log('called:', data.type === "message");
+            if (data.type === "playerJoin" && data.player.name !== this.player.name) {
+                this.chatMessages.push("player joined");
+            }
+
+            if (data.type === "message") {
+                this.receiveChatMessage(data.message);
+                console.log(data.message);
+            }
+
+            if (data.type === "playerLeft") {
+                this.chatMessages.push("player left the room");
+            }
+        },
     },
 
     computed: {
@@ -89,11 +119,27 @@ export default{
 
         piecePlaced() {
             return this.$store.state.board.placed;
-        }
+        },
     },
 
     created() {
         this.generatePieces();
-    }
+    },
+
+    mounted() {
+        this.socket = new WebSocket(`ws://localhost:8000/api/game/ws/${this.$route.game_id}`);
+        this.socket.addEventListener("open", () => {
+            this.socket.send(JSON.stringify({
+                type: "playerJoin", player: {
+                    name: new Date().toString()
+                }
+            }))
+            console.log("joined room");
+        })
+
+        this.socket.addEventListener("message", (event) => {
+            this.processMessage(JSON.parse(event.data));
+        })
+    },
 }
 </script>
