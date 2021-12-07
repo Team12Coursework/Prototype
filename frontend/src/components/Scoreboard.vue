d0649130e755ae8214fd8338b8bccea7cde42169
Harry Lees <harry.lees@gmail.com>
2021-12-07 01:41:40 +0000

    began work on implementing Scoreboard in frontend

--- frontend/src/components/Scoreboard.vue
+++ frontend/src/components/Scoreboard.vue
@@ -1,20 +1,23 @@
 <template>
     <div class="w-full flex rounded justify-center bg-gray-200 p-4">
         <div class="w-1/2 flex justify-around max-w-xl">
-            <p class="font-bold text-6xl">{{ playerOne }}</p>
+            <div class="flex flex-col items-center">
+                <p class="text-xl">{{ points[0].name }}</p>
+                <p class="font-bold text-6xl">{{ points[0].points }}</p>
+            </div>
             <p class="font-bold text-6xl">-</p>
-            <p class="font-bold text-6xl">{{ playerTwo}}</p>
+            <div class="flex flex-col items-center">
+                <p class="text-xl">{{ points[1].name }}</p>
+                <p class="font-bold text-6xl">{{ points[1].points }}</p>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            playerOne: 0,
-            playerTwo: 0,
-        }
+    props: {
+        points: Array,
     }
 }
 </script>
