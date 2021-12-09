d21dc4017f4568ffb3d82a54fe9519f13e28e17f
Harry Lees <harry.lees@gmail.com>
2021-12-09 04:18:27 +0000

    removed previousBoard and added updateBoard mutation

--- frontend/src/store/board.module.js
+++ frontend/src/store/board.module.js
@@ -14,11 +14,9 @@ const initialState = {
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     ],
-    turn: 1,
-    previousBoard: null,
-    placed: false,
+    placed: false
 }
 
 export const board = {
@@ -27,29 +25,14 @@ export const board = {
 
     actions: {
         movePiece({ commit, state }, piece) {
-            let newBoard = state.board;
-            newBoard[piece.y][piece.x] = {
-                id: piece.id,
-                letter: piece.letter
-            };
-            commit('pieceMovedSuccess', newBoard);
-        },
-
-        nextTurn({ commit }) {
-            commit('nextTurnSuccess');
+            let newBoard = JSON.parse(JSON.stringify(state.board));
+            newBoard[piece.y][piece.x] = piece.letter;
+            commit("updateBoard", newBoard);
         },
     },
 
     mutations: {
-        nextTurnSuccess(state) {
-            // function to increment the turn by one, and to store a copy of the
-            // previous turn's board
-            state.previousBoard = state.board;
-            state.turn += 1;
-            state.placed = false;
-        },
-
-        pieceMovedSuccess(state, board) {
+        updateBoard(state, board) {
             state.board = board;
             state.placed = true;
         }
