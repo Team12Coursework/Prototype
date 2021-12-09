5990ee480e34c6bb79bba3640d0e39fa0ed9cc9b
Harry Lees <harry.lees@gmail.com>
2021-12-09 04:17:41 +0000

    removed card_id and interface directly with letter

--- frontend/src/components/Card.vue
+++ frontend/src/components/Card.vue
@@ -3,9 +3,8 @@
     Card component used to style the Scrabble pieces.
     -->
     <div :id="id" :draggable="draggable"
-        class="font-bold w-full h-full flex items-center justify-center bg-blue-500 text-gray-50 cursor-pointer rounded"
-        @dragstart="dragStart" @dragover.stop
-    >
+        class="font-bold w-full h-full flex items-center justify-center text-white bg-gradient-to-br from-blue-600 to-purple-600 cursor-pointer rounded"
+        @dragstart="dragStart" @dragover.stop>
         <p class="w-full h-full flex items-center justify-center">
             {{ letter }}
         </p>
@@ -28,12 +27,11 @@ export default{
             required: true,
         },
     },
-    
+
     methods: {
         dragStart(e) {
             const target = e.target;
-
-            e.dataTransfer.setData('card_id', target.id);
+            e.dataTransfer.setData('letter', this.letter);
 
             setTimeout(() =>{
                 target.style.display = "none";
