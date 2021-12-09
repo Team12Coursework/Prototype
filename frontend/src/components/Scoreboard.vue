e806a7f216ed8e9ade4108c3748e322337cd14c0
Harry Lees <harry.lees@gmail.com>
2021-12-09 04:17:59 +0000

    changed Scoreboard to take in gameData from backend GameManager

--- frontend/src/components/Scoreboard.vue
+++ frontend/src/components/Scoreboard.vue
@@ -2,13 +2,13 @@
     <div class="w-full flex rounded justify-center bg-gray-200 p-4">
         <div class="w-1/2 flex justify-around max-w-xl">
             <div class="flex flex-col items-center">
-                <p class="text-xl">{{ points[0].name }}</p>
-                <p class="font-bold text-6xl">{{ points[0].points }}</p>
+                <p class="text-xl">{{ players[0].name }}</p>
+                <p class="font-bold text-6xl">{{ players[0].points }}</p>
             </div>
             <p class="font-bold text-6xl">-</p>
             <div class="flex flex-col items-center">
-                <p class="text-xl">{{ points[1].name }}</p>
-                <p class="font-bold text-6xl">{{ points[1].points }}</p>
+                <p class="text-xl">{{ players[1].name }}</p>
+                <p class="font-bold text-6xl">{{ players[1].points }}</p>
             </div>
         </div>
     </div>
@@ -17,7 +17,7 @@
 <script>
 export default {
     props: {
-        points: Array,
+        players: Array,
     }
 }
 </script>
