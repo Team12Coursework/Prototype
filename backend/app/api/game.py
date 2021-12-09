fb96a6adf0104f9331b594bd351615a643c53ea6
Harry Lees <harry.lees@gmail.com>
2021-12-09 04:15:51 +0000

    removed most functionality and moved core functions to GameManager

--- backend/app/api/game.py
+++ backend/app/api/game.py
@@ -4,92 +4,51 @@ from typing import Dict
 
 from app import schemas
 from app.api.connection_manager import ConnectionManager
-from app.api.game_manager import GameManager
+from app.api.game_manager import GameManager, Player
 from fastapi import APIRouter, WebSocket, WebSocketDisconnect
-from websockets.exceptions import ConnectionClosedOK
+from websockets.exceptions import ConnectionClosedOK, ConnectionClosedError
+import numpy as np
 
 
 router = APIRouter()
 connection_manager = ConnectionManager()
-game_manager = GameManager()
 
 
 """file holds all of the routes for the game behaviour."""
 
+def current_time() -> str:
+    return datetime.datetime.now().strftime('%H:%M:%S')
 
 def filter_message(data: Dict[str, str]) -> Dict[str, str]:
     """function to filter chat messages"""
 
     return json.dumps(data)
 
+def process_next_turn(room_id: str, board: np.ndarray) -> Dict[str, str]:
+    """process the next turn, this function doesn't return anything to the client"""
+    game: GameManager = connection_manager.connected[room_id].game
+    game.next_turn(board)
+    return game.asdict()
 
-async def process_next_turn() -> None:
-    # there are two possible types for the next turn event.
-    # wordPlaced, and turnSkipped. A turnSkipped event can be raised for a multitude of reasons,
-    # the outcome is the same, the turn will be skipped, no points will be awarded.
-
-    # incoming data should be in the format:
-    # {
-    #    'type': 'wordPlaced',
-    #    'newBoard': List[List[str]], # 15 x 15 board
-    # }
-
-    data = json.dumps({
-
-    })
-
-
-async def process_game_start(room) -> None:
-    """function to process the game start"""
-
-    data = json.dumps({
-        'type': 'gameStart',
-        'tiles': [
-            ['a', 'b', 'c', 'd', 'e', 'f', 'g'],
-            ['a', 'b', 'c', 'd', 'e', 'f', 'g']
-        ],
-        'sentAt': datetime.datetime.now().time(),
-        # the first player to join the lobby is always the first to start the game, can change later if necessary.
-        'playersTurn': connection_manager.connected[room][0].name,
-    })
-    await connection_manager.broadcast(room, data)
-
-
-async def process_room_join(websocket, decoded, room) -> None:
+async def process_room_join(websocket, decoded, room_id: str) -> None:
     """function to process a player joining the room"""
+    name = decoded['player']
+    player = Player(name, websocket)
 
-    # incoming data should be in the format:
-    # {
-    #   'type': 'playerJoin',
-    #   'player': {
-    #       'name': str,
-    #   },
-    #   'sentAt': datetime.date,
-    # }
+    print(f'player {player} joined room {room_id}')
 
-    player = schemas.Player.parse_obj(decoded['player'])
-    print(f'player {player} joined room {room}')
+    await connection_manager.join_game(room_id, player)
+    game = connection_manager.connected[room_id]
 
-    # outgoing data should be in the format:
-    # the first player to enter the lobby will be the lobby owner
-    # and will have the first turn.
-    data = json.dumps({
-        'type':         'playerJoin',
-        'players':      [player.name for player in connection_manager.connected[room]],
-        'sentAt':       datetime.datetime.now().time(),
-    })
-
-    await connection_manager.join(room, player, websocket)
-    await connection_manager.broadcast(room, data)
-
-    if len(connection_manager.connected[room]) == 2:
-        await process_game_start(room)
+    if game.full:
+        game.reset()
+    await connection_manager.broadcast(room_id, json.dumps(game.asdict()))
 
 
 @router.websocket("/ws/{room}")
 async def websocket_endpoint(websocket: WebSocket, room: str):
     """method to handle WebSocket events from frontend"""
-    await connection_manager.connect(websocket)
+    await websocket.accept()
     try:
         while True:
             data: str = await websocket.receive_text()
@@ -98,11 +57,8 @@ async def websocket_endpoint(websocket: WebSocket, room: str):
                 await process_room_join(websocket, decoded, room)
             elif decoded['type'] == 'message':
                 msg = filter_message(decoded)
-                print(f'received message: {msg}')
                 await connection_manager.broadcast(room, msg)
-            else:
-                await connection_manager.broadcast(room, data)
-    except (WebSocketDisconnect, ConnectionClosedOK):
+    except (WebSocketDisconnect, ConnectionClosedOK, ConnectionClosedError):
         await connection_manager.disconnect(room, websocket)
         await connection_manager.broadcast(room, json.dumps({'type': 'playerLeft'}))
         print(f'player disconnected')
