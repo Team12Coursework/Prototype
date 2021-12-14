3ff9ea8555c4ad2fb41b0af5e9ec9227bcd1d9e9
Harry Lees <harry.lees@gmail.com>
2021-12-14 02:48:03 +0000

    changed style for main menu to be more inline with game

--- frontend/src/views/MainMenu.vue
+++ frontend/src/views/MainMenu.vue
@@ -1,15 +1,23 @@
 <template>
-    <navbar/>
-    <main class="flex justify-center w-full">
-        <div class="w-full max-w-7xl flex flex-col space-y-12 items-center">
-            <div class="flex flex-col items-center pt-6">
-                <h1 class="text-6xl font-bold">Character</h1>
-                <h1 class="text-6xl font-bold">Connect</h1>
-            </div>
+    <main class="flex flex-col h-screen">
+        <navbar/>
 
-            <div class="w-1/4 flex flex-col justify-center space-y-4" style="min-width: 512px;">
-                <router-link class="button" to='/gamemodes'>Game Modes</router-link>
-                <leaderboard />
+        <div class="w-full border flex flex-grow justify-center flex-col transform -translate-y-12 items-center">
+            <div class="w-full flex flex-col max-w-screen-md">
+                <div class="w-full flex flex-col items-center justify-center space-y-4">
+                    <div class="flex flex-wrap flex-col justify-center items-center pb-24 space-y-2">
+                        <div class="flex space-x-4">
+                            <h1 v-for="letter in firstWord" :key="letter" class="h-36 flex justify-center items-center w-36 rounded-xl text-white text-8xl font-bold text-center bg-blue-400">{{ letter }}</h1>
+                        </div>
+                        <div class="flex space-x-4">
+                            <h1 v-for="letter in secondWord" :key="letter" class="h-36 flex justify-center items-center w-36 rounded-xl text-white text-8xl font-bold text-center bg-blue-400">{{ letter }}</h1>
+                        </div>
+                    </div>
+
+                    <router-link class="button" to='/gamemodes'>New Game</router-link>
+                    <router-link class="button" to='/gamemodes'>Join Game</router-link>
+                    <router-link class="button" to='/gamemodes'>Leaderboard</router-link>
+                </div>
             </div>
         </div>
     </main>
@@ -19,18 +27,23 @@
 
 <script>
     import Navbar from "@/components/Navbar.vue"
-    import Leaderboard from "@/components/Leaderboard.vue"
 
     export default{
         components: {
             Navbar,
-            Leaderboard,
         },
+
+        data() {
+            return {
+                firstWord: "CHARACTER",
+                secondWord: "CONNECT",
+            }
+        }
     }
 </script>
 
 <style scoped>
 .button {
-    @apply border-2 border-blue-500 rounded-lg text-white font-bold p-4 bg-blue-500 hover:text-blue-500 hover:bg-white duration-200
+    @apply border-8 border-blue-500 text-4xl w-full text-center rounded-full text-white font-bold p-8 bg-blue-300 hover:text-blue-500 hover:bg-white duration-200
 }
 </style>
