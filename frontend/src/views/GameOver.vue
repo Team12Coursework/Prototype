4b1c3a45d25019ae8c3d015e76c2babe762f1651
zaieedh <zaieedh3755@gmail.com>
2022-02-19 17:05:26 +0000

    Added design for main menu and game over page

--- frontend/src/views/GameOver.vue
+++ frontend/src/views/GameOver.vue
@@ -1,15 +1,19 @@
 <template>
-    <navbar/>
-
-    <main class="flex justify-center w-full">
-        <div class="w-full max-w-7xl flex flex-col space-y-12">
-            <div>
-                <h1 class="text-xl">Game Over</h1>
-            </div>
-
-            <router-link to='/gamemodes'>Play Again</router-link>
-            <router-link to='/leaderboard'>Leader board</router-link>
-            <router-link to='/'> Return To Main Menu</router-link>
+    <main class="flex flex-col h-screen">
+        <navbar/>
+        <div class="bg-main-man w-full flex flex-grow">
+                <div class="w-full flex justify-around flex-col flex-grow items-center max-w-screen-md mx-auto">
+                <div class="space-y-4">
+                <div class="text-center text-8xl text-white font-bold">
+                    GAME OVER
+                </div>
+                </div>
+                    <div class="flex flex-col space-y-4">
+                        <router-link class="button" to='/gamemodes'>Play Again</router-link>
+                        <router-link class="button" to='/leaderboard'>Leader board</router-link>
+                        <router-link class="button" to='/'> Return To Main Menu</router-link>
+                    </div>
+                </div>
         </div>
     </main>
 </template>
@@ -26,4 +30,14 @@
 </script>
 
 <style scoped>
+
+.button {
+    @apply border-4 text-black border-black text-2xl w-full text-center rounded-full font-bold py-4 px-16 bg-white hover:bg-black hover:text-white duration-200
+}
+
+.bg-main-man{
+    background: url("../assets/bg-go.jpg");
+    background-size: cover;
+}
+
 </style>
--- frontend/src/views/MainMenu.vue
+++ frontend/src/views/MainMenu.vue
@@ -1,24 +1,22 @@
 <template>
     <main class="flex flex-col h-screen">
         <navbar/>
-
-        <div class="w-full border flex flex-grow justify-center flex-col transform -translate-y-12 items-center">
-            <div class="w-full flex flex-col max-w-screen-md">
-                <div class="w-full flex flex-col items-center justify-center space-y-4">
-                    <div class="flex flex-wrap flex-col justify-center items-center pb-24 space-y-2">
-                        <div class="flex space-x-4">
-                            <h1 v-for="letter in firstWord" :key="letter" class="h-36 flex justify-center items-center w-36 rounded-xl text-white text-8xl font-bold text-center bg-blue-400">{{ letter }}</h1>
-                        </div>
-                        <div class="flex space-x-4">
-                            <h1 v-for="letter in secondWord" :key="letter" class="h-36 flex justify-center items-center w-36 rounded-xl text-white text-8xl font-bold text-center bg-blue-400">{{ letter }}</h1>
-                        </div>
-                    </div>
-
-                    <router-link class="button" to='/gamemodes'>New Game</router-link>
-                    <router-link class="button" to='/gamemodes'>Join Game</router-link>
-                    <router-link class="button" to='/gamemodes'>Leaderboard</router-link>
+                <div class="bg-main-man w-full flex flex-grow">
+                <div class="w-full flex justify-around flex-col flex-grow items-center max-w-screen-md mx-auto">
+                <div class="space-y-4">
+                <div class="flex justify-center space-x-2 logo-container">
+                    <img src="../assets/Character_Connect_Logo.jpg" />
+                    <img class="hand" src="../assets/hand.png" />
+                    <img class="bulb" src="../assets/bulb.png" />
                 </div>
-            </div>
+                </div>
+                <div class="flex flex-col space-y-4">
+                <router-link class="button" to='/gamemodes'>New Game</router-link>
+                <router-link class="button" to='/gamemodes'>Join Game</router-link>
+                <router-link class="button" to='/gamemodes'>Leaderboard</router-link>
+                </div>
+                </div>
+                <img class="bag" src="../assets/letterBag.png" />
         </div>
     </main>
 </template>
@@ -31,19 +29,45 @@
     export default{
         components: {
             Navbar,
-        },
-
-        data() {
-            return {
-                firstWord: "CHARACTER",
-                secondWord: "CONNECT",
-            }
         }
     }
 </script>
 
 <style scoped>
 .button {
-    @apply border-8 border-blue-500 text-4xl w-full text-center rounded-full text-white font-bold p-8 bg-blue-300 hover:text-blue-500 hover:bg-white duration-200
+    @apply border-4 text-black border-black text-2xl w-full text-center rounded-full font-bold py-4 px-16 bg-white hover:bg-black hover:text-white duration-200
+}
+.bg-main-man{
+	background: url("../assets/menu.png");
+	background-size: cover;
+}
+.logo-container{
+	position: relative;
+}
+.logo-container .hand{
+	position: absolute;
+	left: -268px;
+	transition: 0.3s all;
+}
+
+.logo-container .hand:hover{
+	transform: rotate(10deg);
+}
+
+.logo-container .bulb{
+	position: absolute;
+	right: -134px;
+    height: 100%;
+    transition: 0.3s all;
+}
+
+.logo-container .bulb:hover{
+	transform: scale(1.1);
+}
+
+.bag{
+	position: fixed;
+	left: -10px;
+    bottom: -200px;
 }
 </style>
