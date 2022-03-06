a6b79cdd0bc81944e46bc1136b5a0ee804b590a4
zaieedh <zaieedh3755@gmail.com>
2022-03-06 22:43:53 +0000

    Changed design of game modes page

--- frontend/src/components/GoBack.vue
+++ frontend/src/components/GoBack.vue
@@ -2,8 +2,8 @@
     <!-- Simple GoBack component allows the user to return to the page they were previously on, 
     this is used in several places throughout the project -->
 
-    <span class="p-2 rounded text-white bg-blue-500 border-2 border-blue-500 hover:text-blue-500 hover:bg-white">
-        <button @click="goBack">Return</button>
+    <span class="p-2 rounded cstm-button text-white bg-blue-500 border-2 border-blue-500 hover:text-blue-500 hover:bg-white">
+        <button @click="goBack" class="flex items-center"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12l4.58-4.59z"/></svg>Return</button>
     </span>
 </template>
 
@@ -16,3 +16,12 @@ export default{
     }
 }
 </script>
+
+<style>
+.cstm-button svg{
+    fill: white;
+}
+.cstm-button:hover svg{
+    fill: rgb(59 130 246);
+}
+</style>
--- frontend/src/views/GameModes.vue
+++ frontend/src/views/GameModes.vue
@@ -1,37 +1,93 @@
 <template>
-    <navbar/>
+    <main class="flex flex-col h-screen">
+        <navbar/>
+        <div class="bg-main-man w-full flex flex-grow">
+                <div class="w-full flex justify-around flex-col flex-grow items-center max-w-screen-md mx-auto">
+                <div class="space-y-4">
+                <div class="text-center text-6xl font-bold game-over font-effect-anaglyph text-black">
+                    SELECT A GAME MODE
+                </div>
+                </div>
+                    <div class="flex flex-col space-y-4">
+                        <router-link class="button general" to="/lobby"><img class="feature-for-kids" src="../assets/happy.gif" />GENERAL</router-link>
+                <router-link class="button cursor-not-allowed math" to=""><img class="feature-for-kids" src="../assets/mathematics.png" />MATHEMATICS (LOCKED)</router-link>
+                <router-link class="button cursor-not-allowed science" to=""><img class="feature-for-kids" src="../assets/science.png" />SCIENCE (LOCKED)</router-link>
+                <router-link class="button cursor-not-allowed custom" to=""><img class="feature-for-kids" src="../assets/happy.gif" />CUSTOM (LOCKED)</router-link>
+                    </div>
+                </div>
 
-    <main class="flex flex-col w-full items-center pt-6">
-            <h1 class="text-6xl font-bold">Select A Game Mode</h1>
-
-            <div class="w-1/2 max-w-xl flex flex-col items-center space-y-2 mt-4">
-                <router-link class="button" to="/lobby">General</router-link>
-                <router-link class="button cursor-not-allowed" to="">Mathematics (LOCKED)</router-link>
-                <router-link class="button cursor-not-allowed" to="">Science (LOCKED)</router-link>
-                <router-link class="button cursor-not-allowed" to="">Custom (LOCKED)</router-link>
-            </div>
-
-            <go-back class="mt-2" />
+                <go-back class="return-button mt-2" />
+        </div>
     </main>
-    
 </template>
 
-
 <script>
-    import Navbar from '@/components/Navbar.vue'
-    import GoBack from '../components/GoBack.vue'
+    import Navbar from "@/components/Navbar.vue"
+    import GoBack from '@/components/GoBack.vue'
 
     export default{
-        components:{
+        components: {
             Navbar,
-            GoBack,
+            GoBack
         }
     }
+
 </script>
 
-
 <style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Shadows+Into+Light&effect=anaglyph');
 .button {
-    @apply p-2 w-full border-2 border-blue-500 bg-blue-500 text-white hover:bg-white hover:text-blue-500 rounded-lg duration-200
+    @apply text-white text-2xl w-full text-center rounded-full font-bold py-4 px-16 hover:text-white duration-200
 }
+.button{
+    font-family: 'Shadows Into Light', cursive;
+	position: relative;
+	background-size: cover!important;
+	background-repeat: no-repeat!important;
+}
+.button:hover .feature-for-kids{
+	right: -50px;
+	height: 80px;
+	z-index: 1;
+}
+.button.math{
+	background:url('../assets/maths_page.jpg');
+}
+.button.science{
+	background:url('../assets/science_page.jpg');
+}
+.button.custom{
+	background:url('../assets/pattern-colors.jpg');
+}
+.button.general{
+	background: #D44D5C;
+	color: white;
+}
+.game-over{
+    font-family: 'Shadows Into Light', cursive;
+}
+
+.bg-main-man{
+    background-color: #141414;
+    background-image: url(../assets/bg-go-3.jpg);
+    background-size: cover;
+    position: relative;
+}
+
+.return-button{
+	position: absolute;
+	bottom: 20px;
+	left: 20px;
+}
+
+.feature-for-kids{
+	position: absolute;
+	bottom: 0;
+	right: 0;
+	height: 0;
+	width: auto;
+	z-index: -1;
+	transition: 0.3s all;
+}
+
 </style>
