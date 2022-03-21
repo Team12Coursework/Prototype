b03fce6d7200193ed2f39ed6d69521733b97a182
Harry Lees <harry.lees@gmail.com>
2022-03-21 23:06:55 +0000

    fixed assets links to use images in new folder, updated script tag to use new setup API, refactored some HTML to make more visually appealing

--- frontend/src/views/GameModes.vue
+++ frontend/src/views/GameModes.vue
@@ -2,92 +2,91 @@
     <main class="flex flex-col h-screen">
         <navbar/>
         <div class="bg-main-man w-full flex flex-grow">
-                <div class="w-full flex justify-around flex-col flex-grow items-center max-w-screen-md mx-auto">
-                <div class="space-y-4">
-                <div class="text-center text-6xl font-bold game-over font-effect-anaglyph text-black">
-                    SELECT A GAME MODE
-                </div>
-                </div>
-                    <div class="flex flex-col space-y-4">
-                        <router-link class="button general" to="/lobby"><img class="feature-for-kids" src="../assets/happy.gif" />GENERAL</router-link>
-                <router-link class="button cursor-not-allowed math" to=""><img class="feature-for-kids" src="../assets/mathematics.png" />MATHEMATICS (LOCKED)</router-link>
-                <router-link class="button cursor-not-allowed science" to=""><img class="feature-for-kids" src="../assets/science.png" />SCIENCE (LOCKED)</router-link>
-                <router-link class="button cursor-not-allowed custom" to=""><img class="feature-for-kids" src="../assets/happy.gif" />CUSTOM (LOCKED)</router-link>
-                    </div>
-                </div>
+            <div class="w-full flex justify-around flex-col flex-grow items-center max-w-screen-md mx-auto">
+            <div class="space-y-4">
+            <div class="text-center text-6xl font-bold game-over font-effect-anaglyph text-black">
+                SELECT A GAME MODE
+            </div>
 
-                <go-back class="return-button mt-2" />
+            </div>
+                <div class="flex flex-col space-y-4">
+                    <router-link class="button general" to="/lobby"><img class="feature-for-kids" src="@/assets/images/happy.gif" />GENERAL</router-link>
+                    <router-link class="button cursor-not-allowed math" to=""><img class="feature-for-kids" src="@/assets/images/mathematics.png" />MATHEMATICS (LOCKED)</router-link>
+                    <router-link class="button cursor-not-allowed science" to=""><img class="feature-for-kids" src="@/assets/images/science.png" />SCIENCE (LOCKED)</router-link>
+                    <router-link class="button cursor-not-allowed custom" to=""><img class="feature-for-kids" src="@/assets/images/happy.gif" />CUSTOM (LOCKED)</router-link>
+                </div>
+            </div>
+
+            <go-back class="return-button mt-2" />
         </div>
     </main>
 </template>
 
-<script>
-    import Navbar from "@/components/Navbar.vue"
-    import GoBack from '@/components/GoBack.vue'
-
-    export default{
-        components: {
-            Navbar,
-            GoBack
-        }
-    }
-
+<script setup>
+import Navbar from "@/components/Navbar.vue"
+import GoBack from "@/components/GoBack.vue"
 </script>
 
 <style scoped>
 @import url('https://fonts.googleapis.com/css2?family=Shadows+Into+Light&effect=anaglyph');
+
 .button {
     @apply text-white text-2xl w-full text-center rounded-full font-bold py-4 px-16 hover:text-white duration-200
 }
 .button{
     font-family: 'Shadows Into Light', cursive;
-	position: relative;
-	background-size: cover!important;
-	background-repeat: no-repeat!important;
+    position: relative;
+    background-size: cover!important;
+    background-repeat: no-repeat!important;
 }
+
 .button:hover .feature-for-kids{
-	right: -50px;
-	height: 80px;
-	z-index: 1;
+    right: -50px;
+    height: 80px;
+    z-index: 1;
 }
+
 .button.math{
-	background:url('../assets/maths_page.jpg');
+    background:url('../assets/images/maths_page.jpg');
 }
+
 .button.science{
-	background:url('../assets/science_page.jpg');
+    background:url('../assets/images/science_page.jpg');
 }
+
 .button.custom{
-	background:url('../assets/pattern-colors.jpg');
+    background:url('../assets/images/pattern-colors.jpg');
 }
+
 .button.general{
-	background: #D44D5C;
-	color: white;
+    background: #D44D5C;
+    color: white;
 }
+
 .game-over{
     font-family: 'Shadows Into Light', cursive;
 }
 
 .bg-main-man{
     background-color: #141414;
-    background-image: url(../assets/bg-go-3.jpg);
+    background-image: url(../assets/images/bg-go-3.jpg);
     background-size: cover;
     position: relative;
 }
 
 .return-button{
-	position: absolute;
-	bottom: 20px;
-	left: 20px;
+    position: absolute;
+    bottom: 20px;
+    left: 20px;
 }
 
 .feature-for-kids{
-	position: absolute;
-	bottom: 0;
-	right: 0;
-	height: 0;
-	width: auto;
-	z-index: -1;
-	transition: 0.3s all;
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    height: 0;
+    width: auto;
+    z-index: -1;
+    transition: 0.3s all;
 }
-
 </style>
