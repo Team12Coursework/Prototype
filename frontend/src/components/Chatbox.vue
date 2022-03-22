97c67049c842bd209ddc933da31decea6c74475e
Sufyan <200103608@aston.ac.uk>
2022-03-22 21:53:35 +0000

    changes to chatbox, navbar, added game logo.

--- frontend/src/components/Chatbox.vue
+++ frontend/src/components/Chatbox.vue
@@ -3,19 +3,20 @@
 
     <button class= " rounded-full m-1 font-bold w-36 h-10 border-black border-solid  outline-black"  @click="toggleMuted" :class="buttonColour()">MUTE CHAT</button>
     <form @submit="sendMessage" class="add-form">
-        <div class=" bg-gray-300 border-solid border-2 rounded flex flex-col justify-between h-96 w-90/100 border-black">
+        <div class=" bg-white border-solid border-4 shadow-lg rounded flex flex-col justify-between h-96 w-90/100 border-blue-500">
             <div class="flex flex-col">
-                <p class="border rounded p-2 border-black w-3/4 self-end  mt-20"  v-for="message in messages" :key="message">
+                <p :style="colourMessage(message)" v-for="message in messages" :key="message">
                     [{{ message.sentAt }}]:
                     <span v-if="message.type === 'playerJoin'" class="italic text-gray-600">{{ message.player.name }} joined the room</span>
                     <span v-if="message.type === 'message'">({{ message.fromUser }}) {{ message.message }}</span>
                 </p>
             </div>
-            <input class="border rounded p-2 w-3/4 self-center bg-gray-100 mb-1 " type="text" v-model="text" placeholder="Please type in your message here...." />
+            <input class="border rounded border-black p-2 w-3/4 self-center bg-gray-100 mb-1 " type="text" v-model="text" placeholder="Please type in your message here...." />
         </div>
     </form>
 </template> 
 <script>
+
 export default {
     name: 'Chatbox',
     props: {
@@ -30,6 +31,15 @@ export default {
     },
 
     methods: {
+        colourMessage(message) {
+            if (message.fromUser == this.user) {
+                return "text-red-900"
+            } else {
+                return "text-red-800"
+            }
+
+        },
+        
         sendMessage(e) {
             e.preventDefault();
 
