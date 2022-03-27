6fce822fd00a3da89b550585d7a8529fb1112c40
Harry Lees <harry.lees@gmail.com>
2022-03-27 20:58:31 +0100

    fixed missing export issue in constants.js

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,10 +1,13 @@
 const DEBUG = 1;
+
 const baseURL = DEBUG
     ? "http://localhost:8008"
     // I don't know what the URL of the production server is going to be yet.
     // This will get updated soon.
-    : ""
+    : "";
 
 const baseWebSocketURL = DEBUG
-    ? "ws://localhost:8008/"
-    : ""
+    ? "ws://localhost:8008"
+    : "";
+
+export { baseURL, baseWebSocketURL };
