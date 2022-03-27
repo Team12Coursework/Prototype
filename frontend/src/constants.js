572e6ffc9ae51b8ec02e177edf6fd33674c875e9
Harry Lees <harry.lees@gmail.com>
2022-03-27 20:36:45 +0100

    added constants.js

---
+++ frontend/src/constants.js
@@ -0,0 +1,10 @@
+const DEBUG = 1;
+const baseURL = DEBUG
+    ? "http://localhost:8008"
+    // I don't know what the URL of the production server is going to be yet.
+    // This will get updated soon.
+    : ""
+
+const baseWebSocketURL = DEBUG
+    ? "ws://localhost:8008/"
+    : ""
