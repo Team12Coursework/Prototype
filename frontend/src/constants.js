2da485856536743bb228fcb71c894a3f852ecf4b
Harry Lees <harry.lees@gmail.com>
2022-03-30 00:31:16 +0100

    changed debug address to localhost:8000

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,13 +1,13 @@
 const DEBUG = 1;
 
 const baseURL = DEBUG
-    ? "http://localhost:8008"
+    ? "http://localhost:8000"
     // I don't know what the URL of the production server is going to be yet.
     // This will get updated soon.
     : "";
 
 const baseWebSocketURL = DEBUG
-    ? "ws://localhost:8008"
+    ? "ws://localhost:8000"
     : "";
 
 export { baseURL, baseWebSocketURL };
