90947ad53363681deba19d7c0896c194c9448052
Harry Lees <harry.lees@gmail.com>
2022-03-30 01:40:28 +0100

    1) added function to check that the token given was valid 2) added extra AssertionError check to ensure that all disconnect events are caught

--- backend/characterconnect/api/game.py
+++ backend/characterconnect/api/game.py
@@ -2,12 +2,16 @@ import datetime
 import json
 from typing import Callable, Dict, List, Optional, Any
 
-from fastapi import APIRouter, WebSocket
+from fastapi import APIRouter, WebSocket, Depends
 from starlette.websockets import WebSocketDisconnect
 from websockets.exceptions import ConnectionClosedOK, ConnectionClosedError
+from jose import JWTError, jwt
 
 from .connection_manager import ConnectionManager
+from .dependencies import get_database
 from .game_manager import GameManager, Player, InvalidWordException
+from ..core.config import config
+from ..models import User
 
 
 router = APIRouter()
@@ -19,6 +23,37 @@ connection_manager = ConnectionManager()
 
 def current_time() -> str:
     return datetime.datetime.now().strftime('%H:%M:%S')
+
+def token_valid(token: str, database) -> bool:
+    """function to check if the given JWT token is valid
+    1) check that the token decodes correctly
+    2) check that the decoded token contains data
+    3) check that the contained data is valid
+        3.1) check that the given username is valid
+        3.2) check that the given token isn't expired
+    """
+    # 1) check if the token is a valid JWT token
+    try:
+        payload = jwt.decode(token, config.SECRET_KEY, algorithms=[config.ALGORITHM])
+    except JWTError:
+        return False
+
+    # 2) check if the token contains a username
+    username: str | None = payload.get('sub')
+    if username is None:
+        return False
+
+    # 3.2) check the token is expired, this check is done before the database query
+    # as if this fails, it avoids the overhead of making a database call.
+    exp = payload.get("exp")
+    if exp < datetime.utcnow():
+        return False
+
+    # 3.1) check if the token contains a valid username
+    user = database.query(User).where(username == User.username).first()
+    if user is None:
+        return False
+    return True
 
 async def handle_perk_error(callback: Callable[Any, Any], connection_manager, websocket, *args) -> None:
     try:
@@ -57,8 +92,19 @@ def process_next_turn(room_id: str, board: List[List[Optional[str]]]) -> Dict[st
         return {'type': 'updateError', 'message': 'invalid word placed'}
 
 
-async def process_room_join(websocket, decoded, room_id: str) -> None:
+async def process_room_join(websocket, decoded, room_id: str, database = Depends(get_database)) -> None:
     """function to process a player joining the room"""
+    # TODO: this will cause an issue with the game start. If this player gets
+    # removed from the game, the game still won't be able to start. This isn't
+    # pressing.
+    print("recieved process_room_join event", decoded)
+    if not token_valid(decoded["token"], database):
+        await connection_manager.send_personal_message(
+            json.dumps({"type": "authError", "msg": "The given token is invalid, you will be disconnected from the game"}),
+            websocket)
+        await websocket.close()
+        return
+
     name = decoded['player']
     player = Player(name, websocket)
 
@@ -87,7 +133,6 @@ async def websocket_endpoint(websocket: WebSocket, room: str):
                 await connection_manager.broadcast(room, msg)
             elif decoded['type'] == 'gameUpdate':
                 state: Dict[str, str] = process_next_turn(room, decoded['board'])
-                print(decoded, state)
                 if state['type'] == 'gameUpdate':
                     await connection_manager.broadcast(room, json.dumps(state))
                 elif state['type'] == 'updateError':
@@ -108,3 +153,5 @@ async def websocket_endpoint(websocket: WebSocket, room: str):
         await connection_manager.disconnect(room, websocket)
         print(f'player disconnected')
         await connection_manager.broadcast(room, json.dumps(_room.asdict()))
+    except AssertionError:
+        print("player forcefully disconnected")
