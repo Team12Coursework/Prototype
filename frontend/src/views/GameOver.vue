4886720c09d3481f7dbad43968e192669e80af0c
Harry Lees <harry.lees@gmail.com>
2022-03-30 02:35:15 +0100

    added functionality to accept win/lost params

--- frontend/src/views/GameOver.vue
+++ frontend/src/views/GameOver.vue
@@ -1,32 +1,34 @@
 <template>
     <main class="flex flex-col h-screen">
         <navbar/>
+
         <div class="bg-main-man w-full flex flex-grow">
-                <div class="w-full flex justify-around flex-col flex-grow items-center max-w-screen-md mx-auto">
+            <div class="w-full flex justify-around flex-col flex-grow items-center max-w-screen-md mx-auto">
                 <div class="space-y-4">
-                <div class="text-center text-8xl font-bold game-over font-effect-anaglyph text-black">
-                    GAME OVER
+                    <p class="text-8xl font-bold game-over font-effect-anaglyph">{{ won ? "YOU WON" : "YOU LOST" }}</p>
                 </div>
+
+                <div class="flex flex-col space-y-4">
+                    <router-link class="button bg-blue-400" to='/gamemodes'>PLAY AGAIN</router-link>
+                    <router-link class="button bg-green-400" to='/leaderboard'>LEADERBOARD</router-link>
+                    <router-link class="button bg-red-400" to='/'>RETURN TO MAIN MENU</router-link>
                 </div>
-                    <div class="flex flex-col space-y-4">
-                        <router-link class="button bg-blue-400" to='/gamemodes'>PLAY AGAIN</router-link>
-                        <router-link class="button bg-green-400" to='/leaderboard'>LEADERBOARD</router-link>
-                        <router-link class="button bg-red-400" to='/'>RETURN TO MAIN MENU</router-link>
-                    </div>
-                </div>
+            </div>
         </div>
     </main>
 </template>
 
-<script>
-    import Navbar from "@/components/Navbar.vue"
+<script setup>
+import { ref, onMounted } from "vue";
+import { useRoute } from "vue-router";
+import Navbar from "@/components/Navbar.vue";
 
-    export default{
-        components: {
-            Navbar,
-        }
-    }
+const route = useRoute();
+const won = ref(false);
 
+onMounted(() => {
+    won.value = route.params.won;
+});
 </script>
 
 <style scoped>
